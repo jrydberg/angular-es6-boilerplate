f335f06f5a87d32223e946fa89e1cd70549354c3
Peter Bouda <peter@majdigital.ch>
2016-05-11 10:09:23 +0100

    Now bootstrapping angular app in app.js instead of index.html.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,17 +1,24 @@
 /*
-	Import all Angular components via ES6 imports
+    Import all Angular components via ES6 imports
 */
 import RssFeedModule from './components/RssFeed';
 
 /*
-	Define main module
+    Define main module
 */
 angular.module('rssApp', [
-	RssFeedModule
+    RssFeedModule
 ])
 .controller('MainCtrl', function($scope, $http, RssFeed) {
-	RssFeed.loadFeed($http, 'http://jsonplaceholder.typicode.com/posts')
-	.then(function (feed) {
-		$scope.feed = feed;
-	});
+    RssFeed.loadFeed($http, 'http://jsonplaceholder.typicode.com/posts')
+    .then(function (feed) {
+        $scope.feed = feed;
+    });
+});
+
+/*
+  Bootstrap Angular app
+*/
+angular.element(document).ready(function() {
+    angular.bootstrap(document, ['rssApp']);
 });
